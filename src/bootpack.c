2dfd97736e8398be6c78210aaeea552fb4d8552f
FreeBz <freebz@hananet.net>
2013-01-09 01:09:02 +0900

    팔레트 초기화 하기

--- src/bootpack.c
+++ src/bootpack.c
@@ -1,12 +1,21 @@
 /* 다른 파일로 만든 함수가 있으면 C컴파일러에게 알려줌 */
 
 void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
 
 void HariMain(void)
 {
   int i;
   char *p = (char *) 0xa0000;	/* 번지를 대입 */
 
+  init_palette();
+  
   for (i = 0; i <= 0xffff; i++) {
     p[i] = i & 0x0f;
   }
@@ -14,4 +23,44 @@ void HariMain(void)
   for (;;) {
     io_hlt();  /* 이것으로 naskfunc.nas의 _io_hlt가 실행된다. */
   }
+}
+
+void init_palette(void)
+{
+  static unsigned char table_rgb[16 * 3] = {
+    0x00, 0x00, 0x00,	/* 0: 검은 색 */
+    0xff, 0x00, 0x00,	/* 1: 밝은 적색 */
+    0x00, 0xff, 0x00,	/* 2: 밝은 녹색 */
+    0xff, 0xff, 0x00, 	/* 3: 밝은 노란색 */
+    0x00, 0x00, 0xff,	/* 4: 밝은 청색 */
+    0xff, 0x00, 0xff,	/* 5: 밝은 보라색 */
+    0x00, 0xff, 0xff,	/* 6: 밝은 청색 */
+    0xff, 0xff, 0xff,	/* 7: 흰색 */
+    0xc6, 0xc6, 0xc6,	/* 8: 밝은 회색 */
+    0x84, 0x00, 0x00,	/* 9: 어두운 적색 */
+    0x00, 0x84, 0x00,	/* 10: 어두운 녹색 */
+    0x84, 0x84, 0x00,	/* 11: 어두운 노란색 */
+    0x00, 0x00, 0x84,	/* 12: 군청색 */
+    0x84, 0x00, 0x84,	/* 13: 어두운 보라색 */
+    0x00, 0x84, 0x84,	/* 14: 어두운 청색 */
+    0x84, 0x84, 0x84	/* 15: 어두운 회색 */
+  };
+  set_palette(0, 15, table_rgb);
+  return;
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+  int i, eflags;
+  eflags = io_load_eflags();
+  io_cli();
+  io_out8(0x03c8, start);
+  for (i = start; i <= end; i++) {
+    io_out8(0x03c9, rgb[0] / 4);
+    io_out8(0x03c9, rgb[1] / 4);
+    io_out8(0x03c9, rgb[2] / 4);
+    rgb += 3;
+  }
+  io_store_eflags(eflags);
+  return;
 }
--- src/naskfunc.nas
+++ src/naskfunc.nas
@@ -8,7 +8,10 @@
 ; 오브젝트 파일을 위한 정보
 [FILE "naskfunc.nas"]  ; 소스 파일명 정보
 
-      GLOBAL	  _io_hlt     ; 이 프로그램에 포함된 함수명
+      GLOBAL	  _io_hlt, _io_cli, _io_sti, _io_stihlt
+      GLOBAL	  _io_in8, _io_in16, _io_in32
+      GLOBAL	  _io_out8, _io_out16, _io_out32
+      GLOBAL	  _io_load_eflags, _io_store_eflags
 
 ; 이하는 실제의 함수
 
@@ -17,3 +20,61 @@ _io_hlt: 		; void io_hlt(void);
 	HLT
 	RET
 
+_io_cli:		;void io_cli(void);
+	CLI
+	RET
+
+_io_sti:		;void io_sti(void);
+	STI
+	RET
+
+_io_stihlt:		;void io_stihlt(void);
+	STI
+	HLT
+	RET
+
+_io_in8:		;int io_in8(int port);
+	MOV	EDX, [ESP+4] ;port
+	MOV	EAX, 0
+	IN	AL, DX
+	RET
+
+_io_in16:		;int io_in16(int port);
+	MOV	EDX, [ESP+4] ;port
+	MOV	EAX, 0
+	IN	AX, DX
+	RET
+
+_io_in32:
+	MOV	EDX, [ESP+4]
+	IN	EAX, DX
+	RET
+
+_io_out8:		;void io_out8(int port, int data);
+	MOV	EDX, [ESP+4]    ;port
+	MOV	AL, [ESP+8]	;data
+	OUT	DX, AL
+	RET
+
+_io_out16:
+	MOV	EDX, [ESP+4]
+	MOV	AX, [ESP+8]
+	OUT	DX, AX
+	RET
+
+_io_out32:
+	MOV	EDX, [ESP+4]
+	MOV	EAX, [ESP+8]
+	OUT	DX, EAX
+	RET
+
+_io_load_eflags:	; int io_load_eflags(void)
+	PUSHFD
+	POP	EAX
+	RET
+
+_io_store_eflags:	; void io_store_eflags(int eflags)
+	MOV	EAX, [ESP+4]
+	PUSH	EAX
+	POPFD
+	RET
