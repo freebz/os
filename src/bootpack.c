29cefb5385fcc5a10962308fe06f8f39535cc043
FreeBz <freebz@hananet.net>
2013-01-12 01:32:02 +0900

    마우스 커서 그리기

--- src/bootpack.c
+++ src/bootpack.c
@@ -31,6 +31,9 @@ void boxfill8(unsigned char *, int, unsigned char, int, int, int, int);
 void init_screen(char *vram, int xsize, int ysize);
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
 void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
+void init_mouse_cursor8(char *mouse, char bc);
+void putblock8_8(char *vram, int vxsize, int pxsize,
+		 int pysize, int px0, int py0, char *buf, int bxsize);
 
 struct BOOTINFO {
   char cyls, leds, vmode, reserve;
@@ -41,7 +44,8 @@ struct BOOTINFO {
 void HariMain(void)
 {
   struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
-  char s[40];
+  char s[40], mcursor[256];
+  int mx, my;
 
   init_palette();
   init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
@@ -53,6 +57,11 @@ void HariMain(void)
   sprintf(s, "scrnx = %d", binfo->scrnx);
   putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL8_FFFFFF, s);
 
+  mx = (binfo->scrnx - 16) / 2;
+  my = (binfo->scrny - 28 - 16) / 2;
+  init_mouse_cursor8(mcursor, COL8_008484);
+  putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+  
   for (;;) {
     io_hlt();  /* 이것으로 naskfunc.nas의 _io_hlt가 실행된다. */
   }
@@ -154,6 +163,58 @@ void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s
   for (; *s != 0x00; s++) {
     putfont8(vram, xsize, x, y, c, hankaku + *s * 16);
     x += 8;
+  }
+  return;
+}
+
+void init_mouse_cursor8(char *mouse, char bc)
+/* 마우스 커서를 준비(16x16) */
+{
+  static char cursor[16][16] = {
+    "**************..",
+    "*OOOOOOOOOOO*...",
+    "*OOOOOOOOOO*....",
+    "*OOOOOOOOO*.....",
+    "*OOOOOOOO*......",
+    "*OOOOOOO*.......",
+    "*OOOOOOO*.......",
+    "*OOOOOOOO*......",
+    "*OOOO**OOO*.....",
+    "*OOO*..*OOO*....",
+    "*OO*....*OOO*...",
+    "*O*......*OOO*..",
+    "**........*OOO*.",
+    "*..........*OOO*",
+    "............*OO*",
+    ".............***"
+  };
+
+  int x, y;
+  
+  for (y = 0; y < 16; y++) {
+    for (x = 0; x < 16; x++) {
+      if (cursor[y][x] == '*') {
+	mouse[y * 16 + x] = COL8_000000;
+      }
+      if (cursor[y][x] == 'O') {
+	mouse[y * 16 + x] = COL8_FFFFFF;
+      }
+      if (cursor[y][x] == '.') {
+	mouse[y * 16 + x] = bc;
+      }
+    }
+  }
+  return;
+}
+
+void putblock8_8(char *vram, int vxsize, int pxsize,
+		 int pysize, int px0, int py0, char *buf, int bxsize)
+{
+  int x, y;
+  for (y = 0; y < pysize; y++) {
+    for (x = 0; x < pxsize; x++) {
+      vram[ (py0 + y) *vxsize + (px0 + x)] = buf[y * bxsize + x];
+    }
   }
   return;
 }
