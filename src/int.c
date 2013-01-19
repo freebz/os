9fa29c78382cc6e62e0fe7becbf1db263dfabb9f
FreeBz <freebz@hananet.net>
2013-01-19 09:39:49 +0900

    키 코드 취득

--- src/int.c
+++ src/int.c
@@ -1,5 +1,7 @@
 #include "bootpack.h"
 
+#define PORT_KEYDAT	0x0060
+
 void init_pic(void)
 {
   io_out8(PIC0_IMR, 0xff);	/* 모든 인터럽트를 막는다. */
@@ -25,12 +27,14 @@ void inthandler21(int *esp)
 /* PS/2 키보드로부터의 인터럽트 */
 {
   struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
-  boxfill8(binfo->vram, binfo->scrnx, COL8_000000, 0, 0, 32 * 8 - 1, 15);
-  putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF,
-		"INT 21 (IRQ-1) : PS/2 keyboard");
-  for (;;) {
-    io_hlt();
-  }
+  unsigned char data, s[4];
+  io_out8(PIC0_OCW2, 0x61);	/* IRQ-01 접수 완료를 PIC에 통지 */
+  data = io_in8(PORT_KEYDAT);
+  sprintf(s, "%02X", data);
+  boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
+  putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+
+  return;
 }
 
 void inthandler2c(int *esp)
