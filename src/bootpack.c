d9fbb495daee1310e965ca005f800c6f070b36ac
FreeBz <freebz@hananet.net>
2013-01-24 00:13:00 +0900

    마우스 데이터 정리:

--- src/bootpack.c
+++ src/bootpack.c
@@ -5,15 +5,20 @@
 
 extern struct FIFO8 keyfifo, mousefifo;
 
+struct MOUSE_DEC {
+  unsigned char buf[3], phase;
+};
+
 void init_keyboard(void);
-void enable_mouse(void);
+void enable_mouse(struct MOUSE_DEC *mdec);
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
 
 void HariMain(void)
 {
   struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
   char s[40], mcursor[256], keybuf[32], mousebuf[128];
   int mx, my, i;
-  unsigned char mouse_dbuf[3], mouse_phase;
+  struct MOUSE_DEC mdec;
 
   init_gdtidt();
   init_pic();
@@ -35,8 +40,7 @@ void HariMain(void)
   sprintf(s, "(%d, %d)", mx, my);
   putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
-  enable_mouse();
-  mouse_phase = 0;	/* 마우스의 0xfa를 기다리고 있는 단계 */
+  enable_mouse(&mdec);
 
   for (;;) {
     io_cli();
@@ -52,26 +56,9 @@ void HariMain(void)
       } else if (fifo8_status(&mousefifo) != 0) {
 	i = fifo8_get(&mousefifo);
 	io_sti();
-	if (mouse_phase == 0) {
-	  /* 마우스의 0xfa를 기다리고 있는 단계 */
-	  if (i == 0xfa) {
-	    mouse_phase = 1;
-	  }
-	} else if (mouse_phase == 1) {
-	  /* 마우스의 1바이트째를 기다리고 있는 단계 */
-	  mouse_dbuf[0] = i;
-	  mouse_phase = 2;
-	} else if (mouse_phase == 2) {
-	  /* 마우스의 2바이트째를 기다리고 있는 단계 */
-	  mouse_dbuf[1] = i;
-	  mouse_phase = 3;
-	} else if (mouse_phase == 3) {
-	  /* 마우스의 3바이트째를 기다리고 있는 단계 */
-	  mouse_dbuf[2] = i;
-	  mouse_phase = 1;
+	if (mouse_decode(&mdec, i) != 0) {
 	  /* 데이터가 3바이트 쌓였으므로 표시 */
-	  sprintf(s, "%02X %02X %02X", mouse_dbuf[0], 
-		  mouse_dbuf[1], mouse_dbuf[2]);
+	  sprintf(s, "%02X %02X %02X", mdec.buf[0], mdec.buf[1], mdec.buf[2]);
 	  boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16,
 		   32 + 8 * 8 - 1, 31);
 	  putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
@@ -112,12 +99,44 @@ void init_keyboard(void)
 #define KEYCMD_SENDTO_MOUSE	0xd4
 #define MOUSECMD_ENABLE		0xf4
 
-void enable_mouse(void)
+void enable_mouse(struct MOUSE_DEC *mdec)
 {
   /* 마우스 유효 */
   wait_KBC_sendready();
   io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
   wait_KBC_sendready();
   io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
-  return;	/* 잘 되면 ACK(0xfa)가 송된되어 온다. */
+  /* 잘 되면 ACK(0xfa)가 송된되어 온다. */
+  mdec->phase = 0; /* 마우스의 0xfa를 기다리고 있는 단계 */
+  return;
+}
+
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
+{
+  if (mdec->phase == 0) {
+    /* 마우스의 0xfa를 기다리고 있는 단계 */
+    if (dat == 0xfa) {
+      mdec->phase = 1;
+    }
+    return 0;
+  }
+  if (mdec->phase == 1) {
+    /* 마우스의 1바이트째를 기다리고 있는 단계 */
+    mdec->buf[0] = dat;
+    mdec->phase = 2;
+    return 0;
+  }
+  if (mdec->phase == 2) {
+    /* 마우스의 2바이트째를 기다리고 있는 단계 */
+    mdec->buf[1] = dat;
+    mdec->phase = 3;
+    return 0;
+  }
+  if (mdec->phase == 3) {
+    /* 마우스의 3바이트째를 기다리고 있는 단계 */
+    mdec->buf[2] = dat;
+    mdec->phase = 1;
+    return 1;
+  }
+  return -1;	/* 여기에 올 일은 없을 것이다. */
 }
