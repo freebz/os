1c027768cf64da3f534eb65ae6c9bd3b84c7a1b3
FreeBz <freebz@hananet.net>
2013-03-27 01:22:31 +0900

    간단하게 문자열 표시하기

--- src/bootpack.c
+++ src/bootpack.c
@@ -4,6 +4,8 @@
 #include "bootpack.h"
 
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title);
+void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b, 
+		       char *s, int l);
 
 void HariMain(void)
 {
@@ -82,18 +84,20 @@ void HariMain(void)
 
   sprintf(s, "(%d, %d)", mx, my);
   putfonts8_asc(buf_back, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
-
   
   sprintf(s, "memory %dMB    free : %dKB",
 	  memtotal / (1024 * 1024), memman_total(memman) / 1024);
   putfonts8_asc(buf_back, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
   sheet_refresh(sht_back, 0, 0, binfo->scrnx, 48);
-
+  
   for (;;) {
     sprintf(s, "%010d", timerctl.count);
+    
     boxfill8(buf_win, 160, COL8_C6C6C6, 40, 28, 119, 43);
     putfonts8_asc(buf_win, 160, 40, 28, COL8_000000, s);
     sheet_refresh(sht_win, 40, 28, 120, 44);
+    
+    //putfonts8_asc_sht(sht_win, 160, 40, COL8_000000, COL8_C6C6C6, s, 10);
 
     io_cli();
     if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) +
@@ -105,9 +109,8 @@ void HariMain(void)
 	i = fifo8_get(&keyfifo);
 	io_sti();
 	sprintf(s, "%02X", i);
-	boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
-	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
-	sheet_refresh(sht_back, 0, 16, 16, 32);
+	putfonts8_asc_sht(sht_back, 0, 16, COL8_FFFFFF, COL8_008484, s, 2);
+
       } else if (fifo8_status(&mousefifo) != 0) {
 	i = fifo8_get(&mousefifo);
 	io_sti();
@@ -123,10 +126,7 @@ void HariMain(void)
 	  if ((mdec.btn & 0x04) != 0) {
 	    s[2] = 'C';
 	  }
-	  boxfill8(buf_back, binfo->scrnx, COL8_008484, 32, 16,
-		   32 + 15 * 8 - 1, 31);
-	  putfonts8_asc(buf_back, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
-	  sheet_refresh(sht_back, 32, 16, 32 + 15 * 8, 32);
+	  putfonts8_asc_sht(sht_back, 32, 16, COL8_FFFFFF, COL8_008484, s, 15);
 
 	  /* 마우스 커서의 이동 */
 	  mx += mdec.x;
@@ -227,5 +227,14 @@ void make_window8(unsigned char *buf, int xsize, int ysize, char *title)
       buf[(5 + y) * xsize + (xsize - 21 + x)] = c;
     }
   }
+  return;
+}
+
+void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b, 
+		       char *s, int l)
+{
+  boxfill8(sht->buf, sht->bxsize, b, x, y, x + l * 8 - 1, y + 15);
+  putfonts8_asc(sht->buf, sht->bxsize, x, y, c, s);
+  sheet_refresh(sht, x, y, x + l * 8, y + 16);
   return;
 }
