7892cc47de29962b87109c87b8ec3ab896655a2e
FreeBz <freebz.dev@gmail.com>
2014-01-17 01:26:00 +0900

    미침내 첫 애플리케이션 실행

--- src/console.c
+++ src/console.c
@@ -12,6 +12,7 @@ void console_task(struct SHEET *sheet, int memtotal)
 
   struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
   struct FILEINFO *finfo = (struct FILEINFO *) (ADR_DISKIMG + 0x002600);
+  struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) ADR_GDT;
 
   int *fat = (int *) memman_alloc_4k(memman, 4 * 2880);
   file_readfat(fat, (unsigned char *) (ADR_DISKIMG + 0x000200));
@@ -185,6 +186,47 @@ void console_task(struct SHEET *sheet, int memtotal)
 		  }
 		}
 	      }
+	      memman_free_4k(memman, (int) p, finfo[x].size);
+	    } else {
+	      /* 파일이 발견되지 않았을 경우 */
+	      putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF,
+				COL8_000000, "File not found.", 15);
+	      cursor_y = cons_newline(cursor_y, sheet);
+	    }
+	    cursor_y = cons_newline(cursor_y, sheet);
+	  } else if (strcmp(cmdline, "hlt") == 0) {
+	    /* hlt.hrb 애플리케이션을 실행 */
+	    for (y = 0; y < 11; y ++) {
+	      s[y] = ' ';
+	    }
+	    s[0] = 'H';
+	    s[1] = 'L';
+	    s[2] = 'T';
+	    s[8] = 'H';
+	    s[9] = 'R';
+	    s[10] = 'B';
+	    for (x = 0; x < 224; ) {
+	      if (finfo[x].name [0] == 0x00) {
+		break;
+	      }
+	      if ((finfo[x].type & 0x18) == 0) {
+		for (y = 0; y < 11; y++) {
+		  if (finfo[x].name[y] != s[y]) {
+		    goto hlt_next_file;
+		  }
+		}
+		break;	/* 파일이 발견되었다. */
+	      }
+	    hlt_next_file:
+	      x++;
+	    }
+	    if (x < 224 && finfo[x].name[0] != 0x00) {
+	      /* 파일이 발견되었을 경우 */
+	      p = (char *) memman_alloc_4k(memman, finfo[x].size);
+	      file_loadfile(finfo[x].clustno, finfo[x].size, p,
+			    fat, (char *)(ADR_DISKIMG + 0x003e00));
+	      set_segmdesc(gdt + 1003, finfo[x].size - 1, (int) p, AR_CODE32_ER);
+	      farjmp(0, 1003 * 8);
 	      memman_free_4k(memman, (int) p, finfo[x].size);
 	    } else {
 	      /* 파일이 발견되지 않았을 경우 */
