15c91a07df06a69285c82c029ccbc97090fb2b40
FreeBz <freebz.dev@gmail.com>
2014-01-16 21:21:09 +0900

    Enter 키 대응

--- src/bootpack.c
+++ src/bootpack.c
@@ -248,6 +248,11 @@ void HariMain(void)
 	  wait_KBC_sendready();
 	  io_out8(PORT_KEYDAT, keycmd_wait);
 	}
+	if (i == 256 + 0x1c) { /* Enter */
+	  if (key_to != 0) { /* 콘솔에 */
+	    fifo32_put(&task_cons->fifo, 10 + 256);
+	  }
+	}
 	/* 커서의 재표시 */
 	if (cursor_c >= 0) {
 	  boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
@@ -407,7 +412,7 @@ void console_task(struct SHEET *sheet)
 {
   struct TIMER *timer;
   struct TASK *task = task_now();
-  int i, fifobuf[128], cursor_x = 16, cursor_c = -1;
+  int i, fifobuf[128], cursor_x = 16, cursor_y = 28, cursor_c = -1;
   char s[2];
 
   fifo32_init(&task->fifo, 128, fifobuf, task);
@@ -456,22 +461,35 @@ void console_task(struct SHEET *sheet)
 	    putfonts8_asc_sht(sheet, cursor_x, 28, COL8_FFFFFF, COL8_000000, " ", 1);
 	    cursor_x -= 8;
 	  }
+	} else if (i == 10 + 256) {
+	  /* Enter */
+	  if (cursor_y < 28 + 112) {
+	    /* 커서를 스페이스로 지운다. */
+	    putfonts8_asc_sht(sheet, cursor_x, cursor_y,
+			      COL8_FFFFFF, COL8_000000, " ", 1);
+	    cursor_y += 16;
+	    /* 프롬프트 표시 */
+	    putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF,
+			      COL8_000000, ">", 1);
+	    cursor_x = 16;
+	  }
 	} else {
 	  /* 일반문자 */
 	  if (cursor_x < 240) {
 	    /* 한 글자 표시하고 나서, 커서를 하나 뒤로 이동 */
 	    s[0] = i - 256;
 	    s[1] = 0;
-	    putfonts8_asc_sht(sheet, cursor_x, 28, COL8_FFFFFF, COL8_000000, s, 1);
+	    putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, s, 1);
 	    cursor_x += 8;
 	  }
 	}
       }
       /* 커서 재표시 */
       if (cursor_c >= 0) {
-	boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+	boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x,
+		 cursor_y, cursor_x + 7, cursor_y + 15);
       }
-      sheet_refresh(sheet, cursor_x, 28, cursor_x + 8, 44);
+      sheet_refresh(sheet, cursor_x, cursor_y, cursor_x + 8, cursor_y + 16);
     }
   }
 }
